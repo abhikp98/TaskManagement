bf85738484a099f15d8a96e7b9c20829fbb9de28
abhikp98 <kpabhinand16@gmail.com>
2025-10-07 20:24:33 +0530

    add View Tasks Feature

--- backend/urls.py
+++ backend/urls.py
@@ -1,6 +1,6 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
@@ -8,5 +8,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/login/', TokenObtainPairView.as_view(), name="login"),
     path('api/login/refresh/', TokenRefreshView.as_view(), name="refresh"),
+    path('', include('tasks.urls'))
     
 ]
